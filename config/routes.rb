4d4e884dfce42de154598cabaacb91743810fb60
melissa160 <melissa160@gmail.com>
2017-04-18 14:47:37 -0500

    implementando index de actores

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
+  get 'actors' => 'actors#index'
+  # get "/actors" , to: "actors#index"
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
