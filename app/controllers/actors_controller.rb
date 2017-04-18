078d152b71b0e2f43901e3422689db24872a73e0
melissa160 <melissa160@gmail.com>
2017-04-18 18:47:42 -0500

    crear actores

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,4 +3,12 @@ class ActorsController < ApplicationController
   def index
     @actors = Actor.all 
   end
+
+  def new
+    @actors = Actor.new
+  end
+
+  def create
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  get 'actors' => 'actors#index'
-  # get "/actors" , to: "actors#index"
+  # get 'actors' => 'actors#index'
+  get "/actors" , to: "actors#index"
+  get "actors/new", to: "actors#new"
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
