c40e06e6756d81f25754409c7c251d177c516d7b
melissa160 <melissa160@gmail.com>
2017-04-18 20:42:34 -0500

    cambiando ruta

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   # get 'actors' => 'actors#index'
-  get "/actors" , to: "actors#index"
+  get "actors" , to: "actors#index"
   get "actors/new", to: "actors#new"
   post "actors/new", to: "actors#create"
   # Example of named route that can be invoked with purchase_url(id: product.id)
